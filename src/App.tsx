cc56c30518f51ab51cd63f8dc1d424e7a3c37116
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-25 20:44:21 +0200

    Add shadcn init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Section from "./components/Section";
 
 function App() {
-  return <Section className="grid place-content-center">hi</Section>;
+  return (
+    <Section className="grid place-content-center">
+      {/* <Tab /> */}
+      <></>
+    </Section>
+  );
 }
 
 export default App;
