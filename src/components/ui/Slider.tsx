f64207a0bec99149b6a141ffba27520397c9a630
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-29 18:34:01 +0200

    Add Slider components

---
+++ src/components/ui/Slider.tsx
@@ -0,0 +1,27 @@
+import usePosition, { getValueOrDefault } from "@/hooks/usePosition";
+import { cn } from "@/lib/utils";
+import { cubicBezier, motion, type MotionProps } from "framer-motion";
+
+export const transition = {
+  duration: 0.15,
+  ease: cubicBezier(0.69, 0.28, 0.75, 1.22),
+};
+
+interface SliderProps extends MotionProps {
+  className?: string;
+  position?: ReturnType<typeof usePosition>["position"];
+}
+const Slider = ({ className, position, ...props }: SliderProps) => {
+  return (
+    <motion.div
+      initial={getValueOrDefault(position?.slider.past)}
+      animate={getValueOrDefault(position?.slider.current)}
+      transition={transition}
+      className={cn("absolute pointer-events-none", className)}
+      {...props}></motion.div>
+  );
+};
+
+Slider.displayName = "Slider";
+
+export default Slider;
