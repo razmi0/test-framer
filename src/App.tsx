61b3bc2b45ed61906b9dd04524b4c93c363c9915
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-05-29 17:28:02 +0200

    Update App.tsx and main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Background from "./components/ui/Background";
 import Section from "./components/ui/Section";
 
 const App = () => {
-  return (
-    <Section className="relative flex flex-col items-center justify-start p-32 gap-8">
-      <Background type="mosaic" />
-    </Section>
-  );
+  return <Section className="relative flex flex-col items-center justify-start p-32 gap-8">yo</Section>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./tailwind/output.css";
+import Background from "./components/ui/Background.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Background type="mosaic" />
   </React.StrictMode>
 );
